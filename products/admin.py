02ad793348c14c0a43985be3dddf43a3a1238891
mattyImry <matteo.imeri@gmail.com>
2021-05-11 13:08:51 +0000

    customised admin

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,31 @@ from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+# here we create this 2 classes to be able to display the product and cat.
+# you can change the order of columns in admin here
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+# sorting product by sku if you put '-sku' invert the sorting
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
