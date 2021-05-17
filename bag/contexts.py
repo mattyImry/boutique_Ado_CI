5b7edf065430be385f68d43764645962809f5601
mattyImry <matteo.imeri@gmail.com>
2021-05-17 09:44:56 +0000

    added add to bag functionality

--- bag/contexts.py
+++ bag/contexts.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 def bag_contents(request):
 
-    bag_items =[]
+    bag_items = []
     total = 0
     product_count = 0
 
@@ -14,7 +14,7 @@ def bag_contents(request):
     else:
         delivery = 0
         free_delivery_delta = 0
-    
+  
     grand_total = delivery + total
 
     context = {
