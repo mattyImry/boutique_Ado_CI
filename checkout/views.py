86822eb82be70fa1512b71fbe08dfd5624c278fb
mattyImry <matteo.imeri@gmail.com>
2021-05-21 14:14:45 +0000

    Completed checkout templates and test it

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reversefrom 
+from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
 from .forms import OrderForm
