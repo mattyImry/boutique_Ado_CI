239331b81817783f5e5c013ac52a58d006cdcfe9
mattyImry <matteo.imeri@gmail.com>
2021-05-25 10:37:02 +0000

    Add checkout succes logic

--- checkout/models.py
+++ checkout/models.py
@@ -34,7 +34,7 @@ class Order(models.Model):
         Upodate grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
@@ -48,7 +48,7 @@ class Order(models.Model):
         if it hasn't been set already
         """
         if not self.order_number:
-            self.order_number = self._generate_order_number
+            self.order_number = self._generate_order_number()
         super().save(*args, **kwargs)
 
     def __str__(self):
@@ -63,8 +63,8 @@ class OrderLineItem(models.Model):
 
     def save(self, *args, **kwargs):
         """
-        Override the original save method to set the order number:
-        if it hasn't been set already
+        Override the original save method to set the lineitem total	
+        and update the order total.
         """
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
